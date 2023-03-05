63e03dc6847379be97cc227663795d29fc046889
Pavel <ReznikovP998@protonmail.com>
2023-03-05 14:47:00 +0200

    creating minimal functionality

--- TestProject/main.js
+++ TestProject/main.js
@@ -1,6 +1,9 @@
+let answer, inputFileContent, quizMap, curQuestion;
 
-let inputFileContent;
 const inputFileElement = document.getElementById('inputFile');
+const getRandom = (min, max) => Math.floor(Math.random() * (max - min + 1) + min); 
+
+const RESET_TIME = 4000;    //clear form
 
 const parseFileQuestionsAndAnswers = function (str) {
     let result = new Map();
@@ -31,17 +34,133 @@ function inputFileHandler(e) {
 
 inputFileElement.addEventListener('change',inputFileHandler);
 
-function changeToMenu() {
+function changeViewToMenu() {
+    let parentElement = inputFileElement.parentElement;
+    let element = document.createElement('div');
+    
+    element.setAttribute('id','questionContainer');
+    parentElement.appendChild(element);
+    parentElement.replaceChild(element, inputFileElement);
+    
+    element = document.createElement('div');
+
+    parentElement.appendChild(element)
+    parentElement.insertBefore(element, document.getElementById('mainScript')).setAttribute('id', 'answersContainer');
+
+    element = document.createElement('div');
+    
+    parentElement.appendChild(element);
+    parentElement.insertBefore(element, document.getElementById('mainScript')).setAttribute('id','messageContainer');
+}
+
+function displayQuestionWithAnswers(question, answers) {
+    const questionContainerElement = document.getElementById('questionContainer');
+    const answersContainerElement = document.getElementById('answersContainer');
+
+    questionContainerElement.appendChild(document.createElement('h1')).appendChild(document.createTextNode(question));
+    
+    for (const answer of answers) {
+
+        answersContainerElement.appendChild(document.createElement('button')).appendChild(document.createTextNode(answer.replace('<correct>','')));
+
+    }
+
+}
+
+function resetQuestionForm() {
+    document.getElementById('questionContainer').removeChild(document.getElementById('questionContainer').childNodes[0]);
+
+    const answersContainerElement = document.getElementById('answersContainer');
+
+    while (answersContainerElement.firstChild !== null) answersContainerElement.removeChild(answersContainerElement.firstChild);
+
+    document.getElementById('messageContainer').removeChild(document.getElementById('messageContainer').childNodes[0]);
+    answer = undefined;
+}
+
+function checkValueInArr(value,arr) {
+    for (const curVal of arr) if (curVal === value) return true;
+    return false;
+}
+
+function buttonsListner(e) {
+    answer = e.target.childNodes[0].nodeValue;
+    console.log(answer);
+}
+
+function waitForAnswer(milisecond) {
+
+    let timerID = setInterval(function () {
+    if (answer === undefined) console.log('waiting for answer');
+    else {
+        clearInterval(timerID);
+        
+    };
+    }, milisecond)
+
+}
+
+function createMessageOnPage(text) {
+
+    document.getElementById('messageContainer').appendChild(document.createElement('h1')).appendChild(document.createTextNode(text));
     
 }
 
+function afterAnswer() {
+    if (checkAnswer(curQuestion)) createMessageOnPage('correct answer');
+    else createMessageOnPage('incorrect answer');   
+}
+
+function checkAnswer (question) {
+    if (quizMap.get(question).indexOf(answer.concat('<correct>')) !== -1) return true;
+    return false;
+}
+
+async function questionAndAnswersController()  {
+    let questions = [ ...quizMap.keys()], availabeQuestions = [ ...quizMap.keys()], i = questions.length;
+
+    while (i) {
+        curQuestion = questions[getRandom(0, availabeQuestions.length - 1)];
+
+        if (checkValueInArr(curQuestion, availabeQuestions)) {
+            availabeQuestions[availabeQuestions.indexOf(curQuestion)] = undefined;
+
+            displayQuestionWithAnswers(curQuestion, quizMap.get(curQuestion));
+
+            document.getElementById('answersContainer').addEventListener('click', buttonsListner);
+
+            while(answer === undefined) {await sleep(5000);};
+
+            document.getElementById('answersContainer').removeEventListener('click',buttonsListner);
+        
+            afterAnswer();
+
+            await sleep(RESET_TIME);
+            //in furure will be button 'next question'
+            resetQuestionForm();
+
+            --i;
+        }
+        
+    }
+
+    createMessageOnPage('quiz have been finished');
+}
+
+const sleep = ms => new Promise(r => setTimeout(r, ms));
+
 let timerID = setInterval(function () {
-    if (inputFileContent === undefined) console.log('waiting for file...');
+    if (inputFileContent === undefined) console.log('waiting for file');
     else {
         console.log('file was loaded');
         clearInterval(timerID);
         
-        let quizMap = parseFileQuestionsAndAnswers(inputFileContent);
+        quizMap = parseFileQuestionsAndAnswers(inputFileContent);
+        changeViewToMenu();
+
+        questionAndAnswersController(quizMap);
+
         console.log(quizMap);
+        console.dir(document);
     }
 },5000);
